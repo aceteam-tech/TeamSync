06ef3e81c26e60e96f88d1b4712029407e2d4332
Juliusz Jakubowski <juliusz.jakubowski@gmail.com>
2019-07-19 23:44:58 +0200

    subscribe

--- helpers/validateMessage.js
+++
@@ -1,7 +0,0 @@
-const validateMessage = (parsedMessageBody) => {
-    console.log({parsedMessageBody}, !parsedMessageBody.text)
-
-    return parsedMessageBody.text.length > 0
-}
-
-export default validateMessage
