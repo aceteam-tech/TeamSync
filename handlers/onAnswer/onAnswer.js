571531853c1abcf38f41ddcbb7c57c1f1039f16b
Oskar Kaminski <oskarkaminski.dev@gmail.com>
2019-07-19 20:36:04 +0200

    Parsing the user

--- handlers/onAnswer/onAnswer.js
+++ handlers/onAnswer/onAnswer.js
@@ -1,8 +1,22 @@
+import fetch from 'node-fetch'
+
+const token = process.env.SLACK_APP_TOKEN
+
 export const lambda = async (event) => {
     console.log({'event': event});
 
+    const userId = JSON.parse(event.body).event.user
+
+    const response = await fetch(`https://slack.com/api/users.profile.get?token=${token}&user=${userId}`)
+    const user = await response.json()
+
+    console.log({'user': user})
+
     return {
         statusCode: 200,
-        body: JSON.stringify({'challenge': JSON.parse(event.body).challenge})
+        body: JSON.stringify({
+            challenge: JSON.parse(event.body).challenge,
+            user
+        })
     }
 }
