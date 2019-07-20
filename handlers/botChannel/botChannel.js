c5048410fdf9edff54ec2296f3f6dd4936b72294
Piotr Zaborowski <piotr.zaborowski@rubinowajaskinia.pl>
2019-07-20 03:37:26 +0200

    Find botChannel

--- handlers/botChannel/botChannel.js
+++ handlers/botChannel/botChannel.js
@@ -2,33 +2,25 @@ import fetch from 'node-fetch'
 import { urlParams } from '../../helpers/urlParams'
 
 const token = process.env.SLACK_APP_TOKEN
+const appId = process.env.APP_ID
+
+const generalParams = {
+    token: token
+}
 
 export const lambda = async (event) => {
 
-    const botParams = {
-        token: token,
-        bot: 'BLL7LGVRN'
-    }
+    const botIdResponse = await fetch(`https://slack.com/api/users.list?${urlParams(generalParams)}`)
 
-    const botInfoResponse = await fetch(`https://slack.com/api/bots.info?${urlParams(botParams)}`,
-        {
-            method: 'POST'
-        })
+    const botIdJson =  await botIdResponse.json()
 
-    const botUserId =  await botInfoResponse.json()
+    const botId = botIdJson.members.find(x=> x.profile.api_app_id === appId)
 
-    const imParams = {
-        token: token
-    }
-
-    const imResponse = await fetch(`https://slack.com/api/im.list?${urlParams(imParams)}`,
-        {
-            method: 'POST'
-        })
+    const imResponse = await fetch(`https://slack.com/api/im.list?${urlParams(generalParams)}`)
 
     const imList =  await imResponse.json()
 
-    const channelId = imList.ims.find(x => x.user === botUserId.bot.user_id).id;
+    const channelId = imList.ims.find(x => x.user === botId.id).id;
 
     console.log(channelId)
 
