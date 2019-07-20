61983376c9028baaa997c874e418511ed0736223
Oskar Kaminski <oskarkaminski.dev@gmail.com>
2019-07-20 12:47:38 +0200

    Correct team id

--- seed/seedTeam.js
+++ seed/seedTeam.js
@@ -1,7 +1,7 @@
 import TeamsTable from '../db/TeamsTable'
 
 export const lambda = async (event) => {
-    await TeamsTable.putTeam('1')
+    await TeamsTable.putTeam('T93SHU600')
 
     return 'A team has been seeded ;)'
 }
